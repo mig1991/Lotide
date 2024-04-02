77d938d60843ac6419db3144b56e32b18daa5975
Miguel Augusto <miguelmasche@gmail.com>
2024-04-01 19:38:12 -0700

    made assert equal function

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🔴🔴🔴🔴🔴 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🟢🟢🟢🟢🟢 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
