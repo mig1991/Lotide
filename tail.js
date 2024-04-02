1d1470415554d36eedaf55c98a06c4e6af11a594
Miguel Augusto <miguelmasche@gmail.com>
2024-04-01 21:44:40 -0700

    asd

--- tail.js
+++ tail.js
@@ -6,19 +6,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// Helper function to check if arrays have the same elements
-const arraysHaveSameElements = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false; // If the arrays have different lengths, they are not equal
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false; // If any elements are different, the arrays are not equal
-    }
-  }
-  return true; // If all elements are equal, the arrays are equal
-};
-
 // Define the tail function
 const tail = function(array) {
   if (array.length <= 1) {
@@ -29,4 +16,6 @@ const tail = function(array) {
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 const tailWords = tail(words); // Capture the return value of the tail function
+
 assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(words.length, tailWords.length); // comparing lengths instead of arrays directly
