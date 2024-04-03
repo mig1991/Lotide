a3c3b9fa3a21e962a70243c31f118495fa0aee2e
Miguel Augusto <miguelmasche@gmail.com>
2024-04-02 20:49:36 -0700

    fixed it, working now

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,25 +1,23 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
+const eqArrays = function (array1, array2) {
 
   if (array1.length !== array2.length) {
-    console.log(`🔴🔴🔴🔴🔴 ${array1} and ${array2} do not match`);
     return false;
   }
 
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      console.log(`🔴🔴🔴🔴🔴 ${array1} and ${array2} do not match`);
-      
+      return false;
     }
   }
-  console.log(`🟢🟢🟢🟢🟢 Assertion Passed: ${array1} === ${array2}`);
-  
-
+  return true;
 };
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log("pass, both arrays are equal");
+  } else {
+    console.log("fail, arrays are not equal");
+  }
+};
+
+assertArraysEqual([1,2,3], [1,2,3]);
