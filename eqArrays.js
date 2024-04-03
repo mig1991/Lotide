79aca7504862d5c2ef9d98c6d79695849708e409
Miguel Augusto <miguelmasche@gmail.com>
2024-04-02 19:48:32 -0700

    fixed

--- eqArrays.js
+++ eqArrays.js
@@ -10,24 +10,24 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(array1, array2) {
 
   if (array1.length !== array2.length) {
-    console.log(`🔴🔴🔴🔴🔴 ${array1} and ${array2} do not match, therefore false`);
+    
     return false;
   }
 
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      console.log(`🔴🔴🔴🔴🔴 ${array1} and ${array2} do not match, therefore false`);
+      
       return false;
     }
   }
-  console.log(`🟢🟢🟢🟢🟢 Assertion Passed: ${array1} === ${array2}`);
+  
   return true;
 
 };
 
 
 // TEST CODE
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // true
