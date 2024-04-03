b05a50e43cf916695010e9b24f568f197e2f7c24
Miguel Augusto <miguelmasche@gmail.com>
2024-04-03 14:36:49 -0700

    fixed check

--- middle.js
+++ middle.js
@@ -39,7 +39,7 @@ const testArray1 = [1, 2, 3, 4, 5, 6, 7]; // should be [4]
 const testArray2 = [0]; // should be []
 const testArray3 = [1, 2, 3, 4, 5, 6]; // should be [3, 4]
 
-console.log(middle(testArray1)); // should be [4]
-console.log(middle(testArray2)); // should be []
-console.log(middle(testArray3)); // should be [3, 4]
+assertArraysEqual(middle(testArray1), [4]); // should be [4]
+assertArraysEqual(middle(testArray2), []); // should be []
+assertArraysEqual(middle(testArray3), [3, 4]); // should be [3, 4]
 //if array is even, return two mid elements
