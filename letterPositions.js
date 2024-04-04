5541e7e9f03acb5c1fffba7cc173d4cd4f837acd
Miguel Augusto <miguelmasche@gmail.com>
2024-04-03 23:15:29 -0700

    added test

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,40 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (array1, array2) {
+
+  if (array1.length !== array2.length) {
+
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+
+      return false;
+    }
+  }
+
+  return true;
+
+};
+
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} and ${array2} are equal`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${array1} and ${array2} are not equal`);
+  }
+};
+
+
+
 const letterPositions = function (sentence) {
   const results = {};
   let i = 0;
@@ -16,3 +53,4 @@ const letterPositions = function (sentence) {
 
 }
 console.log(letterPositions('lighthouse in the house'));
+assertArraysEqual(letterPositions("hello").e, [1]);
