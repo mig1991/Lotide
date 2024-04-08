d5c03a34e4a6d402ccb1fd2e70ab6772fb208bc3
Miguel Augusto <miguelmasche@gmail.com>
2024-04-08 15:58:14 -0700

    fixed eqArray test

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function (array1, array2) {
 
   if (array1.length !== array2.length) {
@@ -26,8 +17,4 @@ const eqArrays = function (array1, array2) {
 };
 
 
-// TEST CODE
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // true
+module.exports = eqArrays;
