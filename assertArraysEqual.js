0009711de2005bd40c56cd6f9ef65ca996a88320
Miguel Augusto <miguelmasche@gmail.com>
2024-04-08 16:08:37 -0700

    modularized assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(array1, array2) {
-
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -20,6 +8,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-assertArraysEqual([1,2,3], [1,2,3]);
-assertArraysEqual([1,2,3,4], [1,2,3,4,5]);
-assertArraysEqual([1,2,"3"], [1,2,3]);
+module.exports = assertArraysEqual;
