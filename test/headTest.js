21f349ebe401d49ef40c185aee344b80396b1f42
Miguel Augusto <miguelmasche@gmail.com>
2024-04-08 16:54:02 -0700

    fixed headTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,14 +1,14 @@
-const assert = require('chai').assert;
-const head   = require('../head');
+const assert = require("chai").assert;
+const head = require("../head");
 
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
-});
-it("returns '5' for ['5']", () => {
-  assert.strictEqual(head(['5']), '5'); 
-});
-it("returns undefined if there is an empty array", () => {
-  assert.strictEqual(head([]), undefined);
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(["5"]), "5");
+  });
+  it("returns undefined if there is an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
 });
