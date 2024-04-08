061732ff081a0606ffea21afb22da47649ff2558
Miguel Augusto <miguelmasche@gmail.com>
2024-04-08 16:28:17 -0700

    added Mocha & Chai to lotide. Yum

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,14 @@
-const head = require("../head");
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5,2,3,4,5,6]), 5);
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+it("returns '5' for ['5']", () => {
+  assert.strictEqual(head(['5']), '5'); 
+});
+it("returns undefined if there is an empty array", () => {
+  assert.strictEqual(head([]), undefined);
+});
