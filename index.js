c83bfceda4c35b0241fb687debe0c6df8db3ebd4
Miguel Augusto <miguelmasche@gmail.com>
2024-04-08 18:45:11 -0700

    used ES6 shorthand syntax to shorten module.exports

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
-const eqArrays = require('./eqArrays');
-const assertEqual = require('./assertEqual');
-const assertArraysEqual = require('./assertArraysEqual');
-const assertObjectsEqual = require('./assertObjectsEqual');
-const countLetters = require('./countLetters');
-const countOnly = require('./countOnly');
-const eqObjects = require('./eqObjects');
-const findKey = require('./findKey');
-const findKeyByValue = require('./findKeyByValue');
-const letterPositions = require('./letterPositions');
-const takeUntil = require('./takeUntil');
-const without = require('./without');
-
-
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const eqArrays = require("./eqArrays");
+const assertEqual = require("./assertEqual");
+const assertArraysEqual = require("./assertArraysEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const eqObjects = require("./eqObjects");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const letterPositions = require("./letterPositions");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  eqArrays: eqArrays,
-  assertEqual: assertEqual,
-  assertArraysEqual: assertArraysEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  letterPositions: letterPositions,
-  takeUntil: takeUntil,
-  without: without
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertEqual,
+  assertArraysEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  takeUntil,
+  without
 };
