32c402957e0cf71a48156f39f2362e9f6f535c92
Miguel Augusto <miguelmasche@gmail.com>
2024-04-08 18:28:53 -0700

    created index

--- test/middleTest.js
+++ test/middleTest.js
@@ -5,7 +5,7 @@ describe('#middle', () => {
   it('[1, 2, 3, 4, 5, 6, 7] should return [4]', () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
   });
-  it('[0] returns []', () => {
+  it('returns an empty array for [0]', () => {
     assert.deepEqual(middle([0]), []);
   });
   it('[1, 2, 3, 4, 5, 6] should return [3, 4]', () => {
