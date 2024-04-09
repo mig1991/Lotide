b838c8d2c1384c1c24b00d1613ecd69df17f1527
Miguel Augusto <miguelmasche@gmail.com>
2024-04-08 18:40:37 -0700

    removed map

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
 const letterPositions = require('./letterPositions');
-const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
@@ -31,7 +30,6 @@ module.exports = {
   findKey: findKey,
   findKeyByValue: findKeyByValue,
   letterPositions: letterPositions,
-  map: map,
   takeUntil: takeUntil,
   without: without
 };
